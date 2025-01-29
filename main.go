3e0538574b776748a3431953ae35bd8a15e613be
Sullyvan Nunes <sullyvan.nunes@yahoo.com.br>
2025-01-29 11:48:59 -0300

    render templates

--- main.go
+++ main.go
@@ -1,31 +1,77 @@
 package main
 
 import (
-	"os"
+	"io"
+	"log"
+	"net/http"
 
 	"github.com/sullyvannunes/todo-list/views"
 )
 
-func main() {
-	// values := []struct {
-	// 	Name string
-	// }{
-	// 	{"Sullyvan"},
-	// 	{"Alice"},
-	// 	{"Darwin"},
-	// }
-	// if err := views.Render(os.Stdout, "lists/index", values); err != nil {
-	// 	panic(err)
-	// }
-
-	// t1 := template.Must(template.New("asdf").Parse(`Root: {{block "A" .}}This is the first template{{end}} {{block "B" .}}This is the second template{{end}}`))
-	views.Render(os.Stdout, "", nil)
+type Renderer interface {
+	Render(io.Writer, string, any) error
+	RenderWithLayout(io.Writer, string, string, any) error
 }
 
-func Ensure(err error) {
-	if err != nil {
-		panic(err)
+type Application struct {
+	view        Renderer
+	httpHandler http.Handler
+}
+
+func NewApplication(view Renderer) *Application {
+	app := &Application{}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/lists", app.ListIndex())
+	mux.HandleFunc("/actions", app.ActionIndex())
+
+	app.httpHandler = mux
+	app.view = view
+
+	return app
+}
+
+func (a *Application) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a.httpHandler.ServeHTTP(w, r)
+}
+
+func (a *Application) ListIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+
+		if err := a.view.Render(w, "lists/index", []int{1, 2, 3, 4, 5}); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
-// ao crar um novo template uma nova estrutura é isntanciada mas sem nenhum conteúdo
+func (a *Application) ActionIndex() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		w.WriteHeader(http.StatusOK)
+
+		if err := a.view.Render(w, "actions/index", nil); err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+func main() {
+	view := views.New(views.ViewConfig{
+		Namespace:             "views",
+		ComponentDir:          "components",
+		IsProductionMode:      false,
+		LayoutDefault:         "application",
+		TemplateFileExtension: ".html",
+	})
+
+	app := NewApplication(view)
+
+	log.Println("running on http://localhost:3030")
+	http.ListenAndServe(":3030", app)
+}
--- views/view.go
+++ views/view.go
@@ -1,33 +1,108 @@
 package views
 
 import (
-	"embed"
 	"fmt"
+	"html/template"
 	"io"
-	"text/template"
+	"os"
+	"strings"
 )
 
-//go:embed lists/*.html components/*.html layouts/*.html
-var fs embed.FS
+type ViewConfig struct {
+	Namespace             string
+	ComponentDir          string
+	IsProductionMode      bool
+	LayoutDefault         string
+	TemplateFileExtension string
+}
 
-var (
-	appTmpl = template.Must(template.ParseFiles("views/layouts/application.html"))
-)
+type View struct {
+	namespace             string
+	componentDir          string
+	isProductionMode      bool
+	layoutDefault         string
+	templateFileExtension string
+}
 
-func Render(w io.Writer, tmplName string, data any) error {
-	tmpl := template.Must(template.New("views").ParseFS(fs, "layouts/application.html", "lists/*.html"))
-
-	for _, t := range tmpl.Templates() {
-		fmt.Println(t.Name())
+func New(config ViewConfig) *View {
+	namespace := config.Namespace + "/"
+	templateFileExtension := ".html"
+	if config.TemplateFileExtension != "" {
+		templateFileExtension = config.TemplateFileExtension
 	}
 
-	Ensure(tmpl.ExecuteTemplate(w, "application.html", data))
+	return &View{
+		namespace:             namespace,
+		componentDir:          namespace + config.ComponentDir,
+		isProductionMode:      config.IsProductionMode,
+		layoutDefault:         config.LayoutDefault,
+		templateFileExtension: templateFileExtension,
+	}
+}
+
+func (v *View) Render(w io.Writer, tmplName string, data any) error {
+	return v.RenderWithLayout(w, v.layoutDefault, tmplName, data)
+}
+
+func (v *View) RenderWithLayout(w io.Writer, layoutName, tmplName string, data any) error {
+	fs := os.DirFS(v.componentDir)
+	comTmpls, err := template.ParseFS(fs, "*"+v.templateFileExtension)
+	if err != nil {
+		return fmt.Errorf("parse component templates: %w", err)
+	}
+
+	tmplPath := strings.Split(tmplName, "/")
+	fs = os.DirFS(v.namespace + tmplPath[0])
+
+	partialTmpls, err := template.ParseFS(fs, "_*"+v.templateFileExtension)
+	if err != nil {
+		return fmt.Errorf("parse component templates: %w", err)
+	}
+
+	t, err := template.ParseFiles(v.namespace + tmplName + v.templateFileExtension)
+	if err != nil {
+		return fmt.Errorf("parse template file: %w", err)
+	}
+
+	newTmpl, err := v.mergeTemplates(t, comTmpls)
+	if err != nil {
+		return fmt.Errorf("merge template and components: %w", err)
+	}
+
+	newTmpl, err = v.mergeTemplates(newTmpl, partialTmpls)
+	if err != nil {
+		return fmt.Errorf("merge template and partials: %w", err)
+	}
+
+	layout, err := template.ParseFiles(v.namespace + layoutName + v.templateFileExtension)
+	if err != nil {
+		return fmt.Errorf("parse layout template: %w", err)
+	}
+
+	finalTmpl, err := v.mergeTemplates(layout, newTmpl)
+	if err != nil {
+		return fmt.Errorf("merge template and layout: %w", err)
+	}
+
+	if err := finalTmpl.Execute(w, data); err != nil {
+		return fmt.Errorf("execute final template: %w", err)
+	}
 
 	return nil
 }
 
-func Ensure(err error) {
+func (v *View) mergeTemplates(dst, src *template.Template) (*template.Template, error) {
+	dstClone, err := dst.Clone()
 	if err != nil {
-		panic(err)
+		return nil, fmt.Errorf("clone dst template: %w", err)
 	}
+
+	for _, t := range src.Templates() {
+		_, err := dstClone.AddParseTree(t.Name(), t.Tree)
+		if err != nil {
+			return nil, fmt.Errorf("add parse tree: %w", err)
+		}
+	}
+
+	return dstClone, nil
 }
