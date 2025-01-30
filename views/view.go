5c1e4a9066fe70fb3fd2ea8da6bd168e899a4f7e
Sullyvan Nunes <sullyvan.nunes@yahoo.com.br>
2025-01-30 11:22:42 -0300

    fix to allow funcs

--- views/view.go
+++ views/view.go
@@ -56,7 +56,7 @@ func (v *View) addTemplates(dst *template.Template, pattern string) error {
 }
 
 func (v *View) Render(w io.Writer, tmplName string, data any) error {
-	return v.RenderWithLayout(w, "views/application.html", tmplName, data)
+	return v.RenderWithLayout(w, "application.html", tmplName, data)
 }
 
 func (v *View) Execute(tmpl *template.Template, w io.Writer, data any) error {
@@ -71,45 +71,56 @@ func (v *View) RenderWithLayout(w io.Writer, layoutName, tmplName string, data a
 		}
 	}
 
-	file, _ := os.Open("views/" + tmplName + ".html")
-	b, _ := io.ReadAll(file)
-	tmpl, err := template.New("views/" + tmplName + ".html").Funcs(template.FuncMap{"Xablau": v.Xablau}).Parse(string(b))
+	layout, err := template.New("Root").Funcs(v.ViewFuncs()).ParseFiles("views/" + layoutName)
+	if err != nil {
+		return fmt.Errorf("parse layout: %w", err)
+	}
+
+	_, err = layout.ParseFiles("views/" + tmplName + ".html")
+	if err != nil {
+		return fmt.Errorf("parse template %s: %w", tmplName, err)
+	}
+
+	fsys := os.DirFS("views")
+	filenames, err := fs.Glob(fsys, "components/*.html")
 	if err != nil {
 		return err
 	}
 
-	if err = v.addTemplates(tmpl, "components/*.html"); err != nil {
-		return fmt.Errorf("add template %s: %w", "components/*.html", err)
+	if len(filenames) != 0 {
+		_, err = layout.ParseFS(os.DirFS("views"), "components/*.html")
+		if err != nil {
+			return fmt.Errorf("parse components: %w", err)
+		}
 	}
 
-	prefix := strings.Split(tmplName, "/")[0]
-	if err = v.addTemplates(tmpl, prefix+"/_*.html"); err != nil {
-		return fmt.Errorf("add template %s: %w", prefix+"/_*.html", err)
-	}
-
-	layout, err := template.ParseFiles(layoutName)
+	actionDir := strings.Split(tmplName, "/")[0]
+	filenames, err = fs.Glob(fsys, actionDir+"/_*.html")
 	if err != nil {
 		return err
 	}
 
-	if err = v.mergeTemplates(layout, tmpl); err != nil {
-		return err
+	if len(filenames) != 0 {
+		_, err = layout.ParseFS(os.DirFS("views"), actionDir+"/_*.html")
+		if err != nil {
+			return fmt.Errorf("parse components: %w", err)
+		}
 	}
 
-	layout.Funcs(template.FuncMap{
-		"Xablau": v.Xablau,
-	})
+	layoutTmpl := layout.Lookup(layoutName)
 
-	if err = v.Execute(layout, w, data); err != nil {
-		return err
+	if err = layoutTmpl.Execute(w, data); err != nil {
+		return nil
 	}
 
-	v.viewsMap[tmplName] = layout
-
+	v.viewsMap[tmplName] = layoutTmpl
 	return nil
 }
 
-func (v *View) Xablau(data any) string {
-	fmt.Printf("%s %+v\n", "this is xablau", data)
-	return "asdf"
+func (v *View) ViewFuncs() template.FuncMap {
+	return template.FuncMap{
+		"Title": func() string {
+			return "Generated title"
+		},
+	}
 }
